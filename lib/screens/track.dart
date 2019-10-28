b0798d509dfcfe1bfcd3d97f575fcab3a94e37d4
Sebastian Schäfer <bschr9uwb@googlemail.com>
2019-10-28 16:38:43 +0100

    First Prototype of Track Meat Page done

--- lib/screens/track.dart
+++ lib/screens/track.dart
@@ -16,10 +16,11 @@ class TrackMeat extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Theme.of(context).accentColor,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.start,
+        body: ListView(
           children: [
+            Center(
+              child: new RoundedButton(text: 'Add Meat'),
+            ),
             MeatData(
               amount: 200,
               datetime: "25.10.2019",
@@ -32,10 +33,42 @@ class TrackMeat extends StatelessWidget {
               type: 'Pork',
               processed: true,
             ),
-            Text(
-              'Hello',
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            )
+            MeatData(
+              amount: 300,
+              datetime: "27.10.2019",
+              type: 'Pork',
+              processed: true,
+            ),
+            MeatData(
+              amount: 300,
+              datetime: "27.10.2019",
+              type: 'Pork',
+              processed: true,
+            ),
+            MeatData(
+              amount: 300,
+              datetime: "27.10.2019",
+              type: 'Pork',
+              processed: true,
+            ),
+            MeatData(
+              amount: 300,
+              datetime: "27.10.2019",
+              type: 'Pork',
+              processed: true,
+            ),
+            MeatData(
+              amount: 300,
+              datetime: "27.10.2019",
+              type: 'Pork',
+              processed: true,
+            ),
+            MeatData(
+              amount: 300,
+              datetime: "27.10.2019",
+              type: 'Pork',
+              processed: true,
+            ),
           ],
         ));
   }
@@ -48,15 +81,82 @@ class MeatData extends StatelessWidget {
   final bool processed;
 
   const MeatData(
-      {Key key, this.type, this.amount, this.datetime, this.processed})
+      {Key key, this.type, this.amount, this.datetime = "Text", this.processed})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      color: Theme.of(context).primaryColor,
-      child: Text('MeatType'),
+    return Center(
+      child: Card(
+        margin: EdgeInsets.only(left: 60, right: 60, bottom: 15),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(type),
+                  Text('${amount.toString()}g'),
+                ],
+              ),
+              subtitle: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('$datetime'),
+                  Text('$processed'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedButton extends StatelessWidget {
+  const RoundedButton(
+      {Key key,
+      this.sideLength = 50,
+      this.margin = 20,
+      this.borderRadius = 25,
+      this.fontSize = 20,
+      this.text = 'Text'})
+      : super(key: key);
+
+  final double sideLength;
+  final double margin;
+  final double borderRadius;
+  final double fontSize;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      height: sideLength,
+      width: sideLength * 3,
+      duration: Duration(seconds: 2),
+      curve: Curves.easeIn,
+      margin: EdgeInsets.all(margin),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(borderRadius),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(borderRadius),
+          splashColor: Theme.of(context).accentColor,
+          onTap: () {},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                text,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
